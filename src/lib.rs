7da195554e0527a39b732c72a7340677e60bc83f
Marcus Mueller <marcus.mueller.678@gmail.com>
2023-08-29 21:32:01 +0200

    move crate doc to readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,4 @@
-//! # as_base
-//! This crate allows directly accessing fields within a trait object similar to C++ public base classes.
-//! No virtual dispatch is involved, the base object always begins at the same address as the enclosing object.
-//! ```
-//! # use as_base::*;
-//! struct BaseType {
-//!     x: u64,
-//! }
-//!
-//! trait MyTrait: AsBase<BaseType> {}
-//!
-//! #[derive(AsBase)]
-//! #[repr(C)]
-//! struct Implementor {
-//!     pub base: BaseType,
-//! }
-//!
-//! impl MyTrait for Implementor {}
-//!
-//! fn main() {
-//!     let x = Implementor {
-//!         base: BaseType { x: 42 },
-//!     };
-//!     let dyn_reference = &x as &dyn MyTrait;
-//!     assert_eq!(dyn_reference.as_base().x, 42)
-//! }
-//! ```
+#![doc = include_str!("../README.md")]
 
 pub use as_base_proc_macro::AsBase;
 
