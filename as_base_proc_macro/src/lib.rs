09a507724e8fcd77aab090e62035988276c335b5
Marcus Mueller <marcus.mueller.678@gmail.com>
2023-08-31 00:00:50 +0200

    more granular traits and pin

--- as_base_proc_macro/src/lib.rs
+++ as_base_proc_macro/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro_crate::{crate_name, FoundCrate};
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, Meta, Path, Visibility};
 
 fn as_base_crate() -> Path {
@@ -9,14 +9,48 @@ fn as_base_crate() -> Path {
     }
 }
 
-/// implements [AsBase] for a struct or tuple struct.
-///
-/// The first field is used as base.
-/// As this makes the field publicly accessible, the field must be declared as `pub` for clarity.
-///
-/// The type must have a `#[repr(C)]` attribute.
 #[proc_macro_derive(AsBase)]
 pub fn derive_as_base(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    derive_as_base_impl(
+        input,
+        &[
+            (true, "AsBaseRef"),
+            (true, "AsBaseMut"),
+            (true, "AsBasePin"),
+            (true, "AsBasePinMut"),
+            (false, "AsBase"),
+        ],
+    )
+}
+
+/// More specific version of [AsBase].
+#[proc_macro_derive(AsBaseRef)]
+pub fn derive_as_base_ref(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    derive_as_base_impl(input, &[(true, "AsBaseRef")])
+}
+
+/// More specific version of [AsBase].
+#[proc_macro_derive(AsBaseMut)]
+pub fn derive_as_base_mut(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    derive_as_base_impl(input, &[(true, "AsBaseMut")])
+}
+
+/// More specific version of [AsBase].
+#[proc_macro_derive(AsBasePin)]
+pub fn derive_as_base_pin(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    derive_as_base_impl(input, &[(true, "AsBasePin")])
+}
+
+/// More specific version of [AsBase].
+#[proc_macro_derive(AsBasePinMut)]
+pub fn derive_as_base_pin_mut(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    derive_as_base_impl(input, &[(true, "AsBasePinMut")])
+}
+
+fn derive_as_base_impl(
+    input: proc_macro::TokenStream,
+    traits: &[(bool, &str)],
+) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let Data::Struct(DataStruct { fields, .. }) = &input.data else {
         panic!("type must be a struct");
@@ -42,8 +76,16 @@ pub fn derive_as_base(input: proc_macro::TokenStream) -> proc_macro::TokenStream
     let base_type = &base.ty;
     let target_type = input.ident;
     let as_base_crate = as_base_crate();
-    quote! {
-        unsafe impl #as_base_crate::AsBase<#base_type> for #target_type {}
-    }
-    .into()
+    traits
+        .iter()
+        .map(|(u, t)| {
+            let unsafe_impl = if *u { quote!(unsafe) } else { quote!() };
+            let trait_ident = format_ident!("{}", t);
+            let tokens: proc_macro::TokenStream = quote! {
+                #unsafe_impl impl #as_base_crate::#trait_ident<#base_type> for #target_type {}
+            }
+            .into();
+            tokens
+        })
+        .collect()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,63 @@
 #![doc = include_str!("../README.md")]
 
+/// Implements [AsBase].
+///
+/// Works on structs and tuple structs.
+/// The first field is used as base.
+/// The type must have a `#[repr(C)]` attribute.
 pub use as_base_proc_macro::AsBase;
+
+pub use as_base_proc_macro::*;
 
 /// Marker trait for base casting.
 ///
-/// Implementors guarantee that an object of type T resides at the start of Self and that it can be safely accessed.
-/// This is what enables all the casting.
+/// This indicates that it is safe to transmute references to `Self` into Refrences to `T`.
+/// This trait on its own introduces no new requirements, it merely collects all the marker
+/// traits for the different kinds of references for convenience.
 ///
-/// Usually you do not want to implement this manually, use the derive macro.
-/// # Safety
-/// It must be safe to cast immutable and mutable references to Self into references to `T` with same lifetime and mutability via slim pointers.
-pub unsafe trait AsBase<T> {}
+/// You can use the derive macro to implement all the marker traits at once.
+pub trait AsBase<T>: AsBaseRef<T> + AsBaseMut<T> + AsBasePin<T> + AsBasePinMut<T> {}
 
-/// Extension methods for [AsBase].
-///
-/// This is automatically implemented for all types implementing [AsBase].
-/// You are not supposed to implement it yourself.
-pub trait AsBaseExt<B>: AsBase<B> {
-    /// casts to the base type.
-    fn as_base(&self) -> &B {
-        unsafe { &*(self as *const Self as *const u8 as *const B) }
-    }
-    /// casts to the base type.
-    fn as_base_mut(&mut self) -> &mut B {
-        unsafe { &mut *(self as *mut Self as *mut u8 as *mut B) }
-    }
+macro_rules! sub_trait {
+    ($Trait:ident,$ExtTrait:ident,$function:ident,$In:ty,$Out:ty) => {
+        #[doc = "Marker trait to indicate that transmuting `"]
+        #[doc =std::stringify!($In)]
+        #[doc = "` to `"]
+        #[doc =std::stringify!($Out)]
+        #[doc = "` is safe.\n\n["]
+        #[doc =std::stringify!($ExtTrait)]
+        #[doc = "] makes use of this."]
+        pub unsafe trait $Trait<T> {}
+
+        #[doc = "Extension method for ["]
+        #[doc =std::stringify!($Trait)]
+        #[doc = "]."]
+        pub trait $ExtTrait<T>: $Trait<T> {
+            fn $function(self: $In) -> $Out {
+                #[allow(clippy::transmute_ptr_to_ref)]
+                unsafe {
+                    std::mem::transmute(&*self as *const Self as *const u8)
+                }
+            }
+        }
+
+        impl<B, T: AsBase<B> + ?Sized> $ExtTrait<B> for T {}
+    };
 }
 
-impl<B, T: AsBase<B> + ?Sized> AsBaseExt<B> for T {}
+sub_trait!(AsBaseRef, AsBaseRefExt, as_base, &Self, &T);
+sub_trait!(AsBaseMut, AsBaseMutExt, as_base_mut, &mut Self, &mut T);
+sub_trait!(
+    AsBasePin,
+    AsBasePinExt,
+    as_base_pin,
+    std::pin::Pin<&Self>,
+    std::pin::Pin<&T>
+);
+sub_trait!(
+    AsBasePinMut,
+    AsBasePinMutExt,
+    as_base_pin_mut,
+    std::pin::Pin<&mut Self>,
+    std::pin::Pin<&mut T>
+);
